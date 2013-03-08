a90143023476d19685b8c779bcb964c14d0c847d
aryann <aryannm@gmail.com>
2013-03-08 03:21:03 +0100

    Slight changes to gukern

--- src/gameObjects/Gukern.java
+++ src/gameObjects/Gukern.java
@@ -2,10 +2,12 @@ package gameObjects;
 
 public class Gukern {
 	public static void main(String[] args) {
-		System.out.println("fackers gonna fack mothernigger");
+		String gukern = "Gukern";
+		System.out.println("fackers gonna fack mothernigger" + gukern);
 		System.out.println("GUKERN3");
-		System.out.println("Gukern har fått barn");
+		System.out.println("Gukern har fått niggerbarn");
 		System.out.println("apepikk");
+		System.out.println("TestGukernHighlight");
 	}
 
 }
