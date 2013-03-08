a04b7c790941b60c05ff35638a2009fa6282829e
Ferd <Ferd@Ferd-PC>
2013-03-08 03:07:49 +0100

    dreit i dufern

--- src/gameObjects/Gukern.java
+++ src/gameObjects/Gukern.java
@@ -5,6 +5,7 @@ public class Gukern {
 		System.out.println("fackers gonna fack mothernigger");
 		System.out.println("GUKERN3");
 		System.out.println("Gukern har fått barn");
+		System.out.println("apepikk");
 	}
 
 }
