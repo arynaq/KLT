f663ac178e2a0f5cd18ef4c333e7b6fe09d6b6c2
aryann <aryannm@gmail.com>
2013-03-08 03:03:55 +0100

    Highlighted?

--- src/gameObjects/Gukern.java
+++ src/gameObjects/Gukern.java
@@ -3,7 +3,8 @@ package gameObjects;
 public class Gukern {
 	public static void main(String[] args) {
 		System.out.println("fackers gonna fack mothernigger");
-		System.out.println("GUKERN");
+		System.out.println("GUKERN3");
+		System.out.println("Gukern har fått barn");
 	}
 
 }
