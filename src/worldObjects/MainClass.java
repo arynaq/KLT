53e7141dc46177e5b8c71d2230266bbb88b41d1d
aryann <aryannm@gmail.com>
2013-03-08 02:30:52 +0100

    Endra struktur, relativ path

--- src/worldObjects/MainClass.java
+++ src/worldObjects/MainClass.java
@@ -2,8 +2,12 @@ package worldObjects;
 
 import acm.graphics.GImage;
 import acm.program.GraphicsProgram;
+
+@SuppressWarnings("serial")
 public class MainClass extends GraphicsProgram{
 	WorldMap dritern = new WorldMap();
+	private final String fileDir = getClass().getResource(
+			"../images/worldmap/").getPath();
 
 	public void run(){
 		drawBoard(14,14);
@@ -15,7 +19,7 @@ public class MainClass extends GraphicsProgram{
 		for(int y=(1*(yDraw)*16); y<16+(yDraw*16); y++) {
 			for(int x=(1*(xDraw)*16); x<16+(xDraw*16); x++) {
 				int gameTile = dritern.getMap(x,y);
-				GImage tile = new GImage("C:/Users/Ferd/workspace/KLT/src/tiles/"+gameTile+".jpg");
+				GImage tile = new GImage(fileDir + gameTile + ".jpg");
 				add(tile, (x-xDraw*16)*16,(y-yDraw*16)*16);
 			}
 		}
