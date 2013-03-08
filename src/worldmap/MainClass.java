2686a9e0f2406a8efac4aa5e0ed62b59c4e384e6
Ferd <Ferd@Ferd-PC>
2013-03-08 22:44:11 +0100

    Oppdatering etter endring i filstruktur. Nye bilder og endringer i worldmap.java

--- src/worldmap/MainClass.java
+++ src/worldmap/MainClass.java
@@ -3,26 +3,31 @@ package worldmap;
 import acm.graphics.GImage;
 import acm.program.GraphicsProgram;
 
-@SuppressWarnings("serial")
 public class MainClass extends GraphicsProgram{
 	WorldMap dritern = new WorldMap();
+	MapGUI mapGui = new MapGUI();
 	private final String fileDir = getClass().getResource(
 			"../images/worldmap/").getPath();
 
 	public void run(){
-		drawBoard(17, 0);
+		constructMap(0, 0);
 	}
 
-	public void drawBoard(int playerX, int playerY) {
-		int xDraw = ((int) Math.floor(playerX / 16.0 / 1.0));
-		int yDraw = ((int) Math.floor(playerY / 16.0 / 1.0));
-		for (int y = (1 * (yDraw) * 16); y < 16 + (yDraw * 16); y++) {
-			for (int x = (1 * (xDraw) * 16); x < 16 + (xDraw * 16); x++) {
-				int gameTile = dritern.getMap(x, y);
-				GImage tile = new GImage(fileDir + gameTile + ".jpg");
-				add(tile, (x - xDraw * 16) * 16, (y - yDraw * 16) * 16);
+	public void constructMap(int playerX, int playerY) {
+		int skjermen[][] = new int[16][16];
+		int xDraw = ((int) Math.floor(playerX / 16.0));
+		int yDraw = ((int) Math.floor(playerY / 16.0));
+		for (int y = (yDraw*16); y < 16 + (yDraw * 16); y++) {
+			for (int x = (xDraw*16); x < 16 + (xDraw * 16); x++) {
+				skjermen[x-(16*xDraw)][y-(16*yDraw)]=dritern.getMap(x, y);
+				drawMap(x-(16*xDraw),y-(16*yDraw),skjermen[x-(16*xDraw)][y-(16*yDraw)]);
 			}
 		}
 	}
+	public void drawMap(int x, int y,int gameTile){
+		GImage tile = new GImage(fileDir + gameTile + ".jpg");
+		add(tile, x * 16, y * 16);
+	}
 }
+
 
